280f741793492b7fb8424d2630e7afdc2668b5cd
consultancy <muzamilraheman123@gmail.com>
2024-02-23 16:46:33 +0530

    Create Messages and Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
 import React from 'react'
-
+import { BrowserRouter,Route,Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Signin from './pages/Signin';
+import SignOut from './pages/SignUp';
+import Profile from './pages/Profile';
+import Abount from './pages/Abount';
 export default function App() {
-  return <h1 className='text-red-500'>App</h1>;
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/sign-in' element={<Signin/>}></Route>
+      <Route path='/sign-up' element={<SignOut/>}></Route>
+      <Route path='/profile' element={<Profile/>}></Route>
+      <Route path='/abount' element={<Abount/>}></Route>
+    </Routes>
+  </BrowserRouter>
 }
